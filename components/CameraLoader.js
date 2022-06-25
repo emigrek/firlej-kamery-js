2001e07c492de3d5b761497b4c6b0a204bd3ea8e
Emigrek <kaioshinbox@gmail.com>
2022-06-25 18:00:11 +0200

    fixed loader

--- components/CameraLoader.js
+++ components/CameraLoader.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 function CameraLoader() {
   return (
-    <div className='absolute inset-0 z-40 bg-white/5 h-full aspect-video'>
+    <div className='absolute inset-0 z-40 bg-white/5 h-full aspect-video animate-pulse'>
         <div className='flex items-center align-middle justify-center text-center aspect-video'>
-            <div className='relative w-32 h-32 opacity-50 animate-pulse'>
+            <div className='relative w-32 h-32 opacity-50'>
                 <Image alt="logo" priority src={`/logo_firlej.png`} layout='fill'/>  
             </div>
         </div>
