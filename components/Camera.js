628e80a911435cd5099137dc27df9d1ab71f1c15
Emigrek <kaioshinbox@gmail.com>
2022-06-25 16:43:00 +0200

    cams are now updated by ?r=

--- components/Camera.js
+++ components/Camera.js
@@ -1,9 +1,26 @@
 import Image from 'next/image'
+import { useState } from 'react';
 
 function Camera({ id }) {
+  const [random] = useState(Math.random());
+  const [loading, setLoading] = useState(true);
+  
+  const handleLoadingEnd = () => {
+    setLoading(false);
+  };
+
   return (
-    <div className='snap-always snap-center relative w-screen h-max aspect-video'>
-        <Image alt={`camera-${id}`} src={`http://jezioro.firlej.pl/images/Kamery/Kamera${id}.jpg#joomlaImage://local-images/Kamery/Kamera${id}.jpg`} layout='fill'/>  
+    <div className='relative w-max mx-auto aspect-video select-none pointer-events-none'>
+      {
+        loading && (
+          <div className='absolute inset-0 z-40 bg-black/80 h-full aspect-video'>
+            <div className='flex items-center align-middle justify-center text-center h-full aspect-video'>
+              <div className='text-8xl animate-pulse'>📷</div>
+            </div>
+          </div>
+        )
+      }
+      <Image onLoad={handleLoadingEnd} alt={`camera-${id}`} src={`http://jezioro.firlej.pl/images/Kamery/Kamera${id}.jpg?r=${random}#joomlaImage://local-images/Kamery/Kamera${id}.jpg`} layout='fill'/>  
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import Head from 'next/head'
 import { useState } from 'react'
 
 import Camera from '../components/Camera'
-import HelperDown from '../components/HelperDown'
 
 export default function Home() {
-  const [scrollTop, setScrollTop] = useState(0);
   const [cams] = useState([
     { id: 4 },
     { id: 10 },
@@ -13,17 +11,12 @@ export default function Home() {
     { id: 12 }
   ]);
 
-  const handleScroll = (e) => {
-    setScrollTop(e.currentTarget.scrollTop);
-  }
-
   return (
     <div>
       <Head>
         <title>Firlej Kamery</title>
       </Head>
-      <div className='w-screen h-screen snap-y overflow-x-hidden snap-mandatory' onScroll={handleScroll}>
-        {  scrollTop < 100 && <HelperDown/>  }
+      <div className='w-screen h-screen grid grid-cols-1 xl:grid-cols-2 bg-black'>
         { 
           cams.map((cam) => 
             <Camera key={cam.id} id={cam.id}/>
