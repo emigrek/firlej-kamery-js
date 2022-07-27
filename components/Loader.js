e1e6546adfff53ee71e760c8e0d47c2085ba55e7
Emigrek <kaioshinbox@gmail.com>
2022-07-27 02:43:27 +0200

    fixed logo sizing

--- components/Loader.js
+++ components/Loader.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Loader() {
   return (
     <div className='absolute inset-0 z-40 bg-[#030303dd] backdrop-blur-sm flex items-center justify-center align-middle'>
-      <div className='relative w-28 h-28 opacity-80 animate-pulse'>
+      <div className='relative w-16 h-16 lg:w-28 lg:h-28 opacity-80 animate-pulse'>
         <Image alt="logo" priority src={`/logo_firlej.png`} layout='fill'/>  
       </div>
     </div>
